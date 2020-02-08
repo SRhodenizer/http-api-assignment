e49cf45367a05d5b9504133fedfcd0f1b8558c11
SRhodenizer <scr3876@g.rit.edu>
2020-02-07 19:31:13 -0500

    Forbidden doesn't show message

--- src/jsonResponses.js
+++ src/jsonResponses.js
@@ -1,22 +1,123 @@
 // function to respond with a json object
 // takes request, response, status code and object to send
 const respondJSON = (request, response, status, object) => {
-  response.writeHead(status, { 'Content-Type': 'application/json' });
-  response.write(JSON.stringify(object));
-  response.end();
+    response.writeHead(status, {
+        'Content-Type': 'application/json',
+    });
+    response.write(JSON.stringify(object));
+    response.end();
 };
 
 // function to respond without json body
 // takes request, response and status code
 const respondJSONMeta = (request, response, status) => {
-  response.writeHead(status, { 'Content-Type': 'application/json' });
-  response.end();
+    response.writeHead(status, {
+        'Content-Type': 'application/json',
+    });
+    response.end();
 };
 
-// function to add a user from a POST body
+// functions for the required requests
+const getSuccess = (request, response, status) => {
+    return respondJSON(request, response, status, {
+        id: status,
+        message: 'This is a successful response',
+    });
+};
+
+const getSuccessMeta = (request, response, status) => {
+    return respondJSONMeta(request, response, status);
+};
+
+const getBadRequest = (request, response, status) => {
+    if (status === 400) {
+        return respondJSON(request, response, status, {
+            id: status,
+            message: 'Missing valid query parameter set to false',
+        });
+    } else if (status === 200) {
+        return respondJSON(request, response, status, {
+            id: status,
+            message: 'Missing valid query parameter set to true',
+        });
+    }
+};
+
+const getBadRequestMeta = (request, response, status) => {
+    return respondJSONMeta(request, response, status);
+};
+
+const getUnauthorized = (request, response, status) => {
+    return respondJSON(request, response, status, {
+        id: status,
+        message: 'Missing loggedIn query parameter set to yes',
+    });
+};
+
+const getUnauthorizedMeta = (request, response, status) => {
+    return respondJSONMeta(request, response, status);
+};
+
+const getForbidden = (request, response, status) => {
+    return respondJSON(request, response, status, {
+        id: status,
+        message: 'You do not have access to this content',
+    });
+};
+
+const getForbiddenMeta = (request, response, status) => {
+    return respondJSONMeta(request, response, status);
+};
+
+
+const getInternal = (request, response, status) => {
+    return respondJSON(request, response, status, {
+        id: status,
+        message: 'Internal Server Error. Something went wrong',
+    });
+};
+
+const getInternalMeta = (request, response, status) => {
+    return respondJSONMeta(request, response, status);
+};
+
+
+const getNotImplemented = (request, response, status) => {
+    return respondJSON(request, response, status, {
+        id: status,
+        message: 'A get request for this page has not been implemented yet. Check again later for updated content',
+    });
+};
+
+const getNotImplementedMeta = (request, response, status) => {
+    return respondJSONMeta(request, response, status);
+};
+
+const getNotFound = (request, response, status) => {
+    return respondJSON(request, response, status, {
+        id: status,
+        message: 'The page you were looking for was not found',
+    });
+};
+
+const getNotFoundMeta = (request, response, status) => {
+    return respondJSONMeta(request, response, status);
+};
 
 // public exports
 module.exports = {
-  respondJSON,
-  respondJSONMeta,
+    getSuccess,
+    getSuccessMeta,
+    getBadRequest,
+    getBadRequestMeta,
+    getUnauthorized,
+    getUnauthorizedMeta,
+    getForbidden,
+    getForbiddenMeta,
+    getInternal,
+    getInternalMeta,
+    getNotImplemented,
+    getNotImplementedMeta,
+    getNotFound,
+    getNotFoundMeta,
 };
--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ const http = require('http');
 // get the url module
 const url = require('url');
 // querystring module for parsing querystrings in the url
-const query = require('querystring');
+// const query = require('querystring');
 // pull in the response finals
 const htmlHandler = require('./htmlResponses.js');
 const jsonHandler = require('./jsonResponses.js');
@@ -11,71 +11,31 @@ const jsonHandler = require('./jsonResponses.js');
 // sets up the port for the server
 const port = process.env.port || process.env.NODE_PORT || 3000;
 
-// handle POST requests
-const handlePost = (request, response, parsedUrl) => {
-    
-    if (parsedUrl.pathname === '/success') {
-        const res = response;
-
-        // uploads come in as a byte stream that we need
-        // to reassemble once it's all arrived
-        const body = [];
-
-        // if the upload stream errors out, just throw a
-        // a bad request and send it back
-        request.on('error', (err) => {
-            console.dir(err);
-            res.statusCode = 400;
-            res.end();
-        });
-
-        // on 'data' is for each byte of data that comes in
-        // from the upload. We will add it to our byte array.
-        request.on('data', (chunk) => {
-            body.push(chunk);
-        });
-
-        // on end of upload stream.
-        request.on('end', () => {
-            // combine our byte array (using Buffer.concat)
-            // and convert it to a string value (in this instance)
-            const bodyString = Buffer.concat(body).toString();
-            // since we are getting x-www-form-urlencoded data
-            // the format will be the same as querystrings
-            // Parse the string into an object by field name
-            const bodyParams = query.parse(bodyString);
-
-            // pass to our addUser function
-            //jsonHandler.addUser(request, res, bodyParams);
-        });
-    }
-};
-
 // handle GET requests
 const handleGet = (request, response, parsedUrl) => {
     // route to correct method based on url
     if (parsedUrl.pathname === '/style.css') {
         htmlHandler.getCSS(request, response);
-    }else if (parsedUrl.pathname === '/') {
+    } else if (parsedUrl.pathname === '/') {
         htmlHandler.getIndex(request, response);
     } else if (parsedUrl.pathname === '/success') {
-        jsonHandler.respondJSON(request,response,200,{id:200,message:"This is a successful response"});
+        jsonHandler.getSuccess(request, response, 200);
     } else if (parsedUrl.pathname === '/badRequest') {
-        jsonHandler.respondJSON(request,response,400,{id:400,message:"Missing valid query parameter set to true"});
+        jsonHandler.getBadRequest(request, response, 400);
     } else if (parsedUrl.pathname === '/badRequest?valid=true') {
-        jsonHandler.respondJSON(request,response,200,{id:200,message:"Missing valid query parameter set to true"});
+        jsonHandler.getBadRequest(request, response, 200);
     } else if (parsedUrl.pathname === '/unauthorized') {
-        jsonHandler.respondJSON(request,response,401,{id:401,message:"Missing loggedIn query parameter set to yes"});
-    }else if (parsedUrl.pathname === '/unauthorized?loggedIn=yes') {
-        jsonHandler.respondJSON(request,response,200,{id:200,message:"Missing loggedIn query parameter set to yes"});
+        jsonHandler.getUnauthorized(request, response, 401);
+    } else if (parsedUrl.pathname === '/unauthorized?loggedIn=yes') {
+        jsonHandler.getUnauthorized(request, response, 200);
     } else if (parsedUrl.pathname === '/forbidden') {
-        jsonHandler.respondJSON(request,response,403,{id:403,message:"You do not have access to this content"});
+        jsonHandler.getForbidden(request, response, 403);
     } else if (parsedUrl.pathname === '/internal') {
-        jsonHandler.respondJSON(request,response,500,{id:500,message:"Internal Server Error. Something went wrong"});
+        jsonHandler.getInternal(request, response, 500);
     } else if (parsedUrl.pathname === '/notImplemented') {
-        jsonHandler.respondJSON(request,response,501,{id:501,message:"A get request for this page has not been implemented yet. Check again later for updated content"});
+        jsonHandler.getNotImplemented(request, response, 501);
     } else {
-        jsonHandler.respondJSON(request,response,404,{id:404,message:"The page you were looking for was not found"});
+        jsonHandler.getNotFound(request, response, 404);
     }
 };
 
@@ -83,14 +43,7 @@ const onRequest = (request, response) => {
     // parse url into individual parts
     // returns an object of url parts by name
     const parsedUrl = url.parse(request.url);
-
-    // check if method was POST, otherwise assume GET
-    // for the sake of this example
-    if (request.method === 'POST') {
-        handlePost(request, response, parsedUrl);
-    } else {
-        handleGet(request, response, parsedUrl);
-    }
+    handleGet(request, response, parsedUrl);
 };
 
 http.createServer(onRequest).listen(port);
